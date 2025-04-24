41a16169f0fa8d684f192f09a226d0fab80d8d75
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-04-24 15:00:27 -0700

    Refactor API to be more compatible

--- pages/api/cookies.js
+++ pages/api/cookies.js
@@ -4,20 +4,22 @@ import { posts } from '../../content'
 export default async function handler(req, res) {
     console.log("Cookies request", req.cookies);
     const lastPostVisit = req.cookies['lastPostVisit'];
-    let initialVisit = req.cookies['initialVisit'];
-
-    if (!initialVisit) {
-        initialVisit = Date.now();
-        res.cookies.set("initialVisit", initialVisit);
-    }
+    let setInitialVisit = null;
+    const initialVisit = req.cookies['initialVisit'] ? req.cookies['initialVisit'] : (setInitialVisit = Date.now());
 
     const count = posts.map(post => post.meta.slug).toReversed().indexOf(lastPostVisit);
 
     console.log("Cookies response", lastPostVisit, initialVisit);
 
-    res.status(200).json({
+    const json = {
         lastPostVisit: lastPostVisit,
         initialVisit: initialVisit,
         count
-    });
+    };
+
+    if (setInitialVisit) {
+        res.status(200).setHeader("Set-Cookie", "initialVisit=" + initialVisit).json(json);
+    } else {
+        res.status(200).json(json);
+    }
 }
