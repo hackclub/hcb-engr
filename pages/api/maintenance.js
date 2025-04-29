dbf2b8ccf8920748fd9451c41209d48cf6242742
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-04-28 22:50:07 -0700

    Swap maintenance to redirect to /posts

--- pages/api/maintenance.js
+++ pages/api/maintenance.js
@@ -2,5 +2,5 @@ import { posts } from '../../content'
 
 export default async function handler(req, res) {
     const maintenance = posts.filter(post => post.meta.category === "maintenance").sort((a, b) => b.meta.date - a.meta.date)[0];
-    res.redirect(301, `/preview/${maintenance.meta.slug}`);
+    res.redirect(301, `/posts/${maintenance.meta.slug}`);
 }
